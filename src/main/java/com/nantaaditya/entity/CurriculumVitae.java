98aa6f9e6fb5082e15a8dabc731f5c04b65015fd
pramuditya anantanur <pramuditza@gmail.com>
2018-06-30 17:55:19 +0700

    add entity CurriculumVitae

---
+++ src/main/java/com/nantaaditya/entity/CurriculumVitae.java
@@ -0,0 +1,29 @@
+package com.nantaaditya.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+// @formatter:off
+/**
+  * Author : Pramuditya Ananta Nur
+  * www.nantaaditya.com
+  * personal@nantaaditya.com
+  **/
+// @formatter:on
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "N_EDUCATION")
+public class CurriculumVitae extends AbstractEntity {
+
+  private String name;
+  private String start;
+  private String end;
+  private String description;
+}
