e753c756850f0fa3bc91d71ecd2e495441305e9d
pramuditya anantanur <pramuditza@gmail.com>
2018-07-15 06:02:45 +0700

    add project command web request

---
+++ src/main/java/com/nantaaditya/model/web/SaveProjectWebRequest.java
@@ -0,0 +1,26 @@
+package com.nantaaditya.model.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotBlank;
+// @formatter:off
+/**
+  * Author : Pramuditya Ananta Nur
+  * www.nantaaditya.com
+  * personal@nantaaditya.com
+  **/
+// @formatter:on
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaveProjectWebRequest implements WebRequest {
+
+  @NotBlank(message = "name is required")
+  private String name;
+  @NotBlank(message = "url is required")
+  private String url;
+}
