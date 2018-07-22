b8207299d80074aeb4ebf5803e72f5c2f87c8c16
pramuditya anantanur <pramuditza@gmail.com>
2018-07-22 17:21:27 +0700

    add message repository

---
+++ src/main/java/com/nantaaditya/repository/MessageRepository.java
@@ -0,0 +1,17 @@
+package com.nantaaditya.repository;
+
+import com.nantaaditya.entity.Message;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+// @formatter:off
+/**
+  * Author : Pramuditya Ananta Nur
+  * www.nantaaditya.com
+  * personal@nantaaditya.com
+  **/
+// @formatter:on
+@Repository
+public interface MessageRepository extends JpaRepository<Message, String> {
+  List<Message> findAllOrderByStateDesc();
+}
