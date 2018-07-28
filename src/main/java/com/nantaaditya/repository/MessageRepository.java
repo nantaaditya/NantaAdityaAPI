646cb979d6259246bfeded9ce84ab6cfaff3a99a
pramuditya anantanur <pramuditza@gmail.com>
2018-07-28 15:11:23 +0700

    update repository

--- src/main/java/com/nantaaditya/repository/MessageRepository.java
+++ src/main/java/com/nantaaditya/repository/MessageRepository.java
@@ -13,5 +13,5 @@ import org.springframework.stereotype.Repository;
 // @formatter:on
 @Repository
 public interface MessageRepository extends JpaRepository<Message, String> {
-  List<Message> findAllOrderByStateDesc();
+  List<Message> findAllByOrderByStateDesc();
 }
