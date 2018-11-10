c5461dee45289459f6038ec148439cea9e3284c7
pramuditya anantanur <pramuditza@gmail.com>
2018-11-10 08:50:42 +0700

    update main class

--- src/main/java/com/nantaaditya/NantaAdityaApplication.java
+++ src/main/java/com/nantaaditya/NantaAdityaApplication.java
@@ -1,14 +1,36 @@
 package com.nantaaditya;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class NantaAdityaApplication {
+public class NantaAdityaApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(NantaAdityaApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(NantaAdityaApplication.class);
+  }
+
+  @Bean
+  public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+    TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+    tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+      if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+        ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+      }
+    });
+    return tomcat;
   }
 }
