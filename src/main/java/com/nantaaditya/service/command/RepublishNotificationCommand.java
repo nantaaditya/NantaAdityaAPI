cef0714cebd97f5be35ee3690b96ebdf35e70443
pramuditya anantanur <pramuditza@gmail.com>
2018-11-10 08:49:36 +0700

    add republish notification

---
+++ src/main/java/com/nantaaditya/service/command/RepublishNotificationCommand.java
@@ -0,0 +1,16 @@
+package com.nantaaditya.service.command;
+
+import com.nantaaditya.model.EmptyResponse;
+import com.nantaaditya.model.command.RepublishNotificationCommandRequest;
+// @formatter:off
+/**
+ * Author : Pramuditya Ananta Nur
+  * www.nantaaditya.com
+  * personal@nantaaditya.com
+  **/
+// @formatter:on
+
+public interface RepublishNotificationCommand extends
+    Command<EmptyResponse, RepublishNotificationCommandRequest> {
+
+}
